653c5c16cdb3fc6549a06e225c677e4757be036c
DBozunov <bazik88@mail.ru>
2018-06-20 21:50:40 +0700

    fill controllers

--- src/main/java/ru/mg/microservices/routehelpers/RouteHelperController.java
+++ src/main/java/ru/mg/microservices/routehelpers/RouteHelperController.java
@@ -1,4 +1,24 @@
 package ru.mg.microservices.routehelpers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mg.microservices.entity.RoutePoint;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
 public class RouteHelperController {
+    private Logger logger = Logger.getLogger(RouteHelperController.class.getName());
+    private RouteHeplerRepository routeHeplerRepository;
+
+    public RouteHelperController(RouteHeplerRepository routeHeplerRepository) {
+        this.routeHeplerRepository = routeHeplerRepository;
+    }
+
+    @GetMapping("/getTimeForRoute")
+    public Integer getTimeForRoute(List<RoutePoint> pointsList) {
+        return -1;
+    }
 }
--- src/main/java/ru/mg/microservices/routes/RoutesController.java
+++ src/main/java/ru/mg/microservices/routes/RoutesController.java
@@ -1,4 +1,37 @@
 package ru.mg.microservices.routes;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mg.microservices.entity.Route;
+import ru.mg.microservices.routehelpers.RouteHelperController;
+import ru.mg.microservices.routehelpers.RouteHeplerRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
 public class RoutesController {
+    private Logger logger = Logger.getLogger(RoutesController.class.getName());
+    private RoutesRepository routesRepository;
+
+    public RoutesController(RoutesRepository routesRepository) {
+        this.routesRepository = routesRepository;
+    }
+
+    @GetMapping("/getRoutesList")
+    public List<Route> getRoutesList() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/getRoute")
+    public Route getRoute(Integer routeId) {
+        return new Route();
+    }
+
+    @PostMapping("/addRoute")
+    public Integer addRoute(List<Integer> routePointsList) {
+        return 0;
+    }
 }
